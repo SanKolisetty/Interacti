20fef3cd510db72ba9fd9d3e8d3ca1ed6bed2f76
Sanjana Kolisetty <95172001+SanKolisetty@users.noreply.github.com>
2023-07-04 15:46:59 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handlesubmit = async (e) =>{
     console.log("hello ")
 
     // fetch data from server
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://interacti.onrender.com', {
         method: 'POST',
         headers:{
             'Content-Type': 'application/json'
